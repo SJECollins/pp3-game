90f7fc101fac7b03328882fc0ac9d219f4dffca6
SJECollins <>
2023-06-23 20:55:53 +0000

    Start making computer smarter

--- games/connectfour.py
+++ games/connectfour.py
@@ -6,6 +6,7 @@ from helpers.helpers import Colours, get_input
 
 player_one = f"{Colours.YELLOW}\u2B24{Colours.END}"
 player_two = f"{Colours.RED}\u2B24{Colours.END}"
+space = f"{Colours.GREY}\u2B24{Colours.END}"
 bar = f"{Colours.BLUE}|{Colours.END}"
 
 
@@ -16,7 +17,8 @@ def print_board(board):
     print(ends)
     num = 1
     for row in board:
-        print("{:^75}".format(f"%d {bar}%s{bar}") % (num, f"{bar}".join(row)))
+        print("{:^75}".format(f"%d {bar}%s{bar}")
+              % (num, f"{bar}".join(row)))
         if num < 6:
             print(mids)
         num += 1
@@ -34,24 +36,96 @@ def get_user_move(board):
         user_col = int(col) - 1
 
         for row in range(6):
-            if board[row][user_col] == " ":
+            if board[row][user_col] == space:
                 user_row = row
 
         move = user_row, user_col
-        if board[user_row][user_col] == " ":
+        if board[user_row][user_col] == space:
             return move
         print("That's already taken.")
 
 
-def get_comp_move(board):
-    while True:
-        comp_col = randint(0, 6)
-        for row in range(6):
-            if board[row][comp_col] == " ":
-                comp_row = row
-        move = comp_row, comp_col
-        if board[comp_row][comp_col] == " ":
-            return move
+def get_comp_move(board, comp):
+    print("comp")
+    comp_col = 0
+    comp_row = 0
+    target = False
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == comp:
+                # Check below, go above
+                if (row > 0 and board[row - 1][col] == space):
+                    target = True
+                    comp_col = col
+                    print("above")
+                    break
+                # Check right, go left
+                if (col > 0 and board[row][col + 1] == comp
+                        and board[row][col - 1] == space
+                        and board[row + 1][col - 1] != space):
+                    target = True
+                    comp_col = col - 1
+                    print("left")
+                    break
+                # Go right
+                if (col < 7 and board[row][col + 1] == space
+                        and board[row + 1][col + 1] != space):
+                    target = True
+                    comp_col = col + 1
+                    print("right")
+                    break
+                # Check below left, go above right
+                if (row > 0 and col < 7 and board[row + 1][col - 1] == comp
+                        and board[row][col + 1] != space
+                        and board[row - 1][col + 1] == space):
+                    target = True
+                    comp_col = col + 1
+                    print("above right")
+                    break
+                # Check below right, go above left
+                if (row > 0 and col > 0 and board[row + 1][col + 1] == comp
+                        and board[row][col - 1] != space
+                        and board[row - 1][col - 1] == space):
+                    target = True
+                    comp_col = col - 1
+                    print("above left")
+                    break
+                # Go below left
+                if (col > 0 and board[row + 1][col - 1] == space
+                        and board[row + 2][col - 1] != space):
+                    target = True
+                    comp_col = col - 1
+                    print("below left")
+                    break
+                
+                # Go below right
+                if (col < 7 and board[row + 1][col + 1] == space
+                        and board[row + 2][col - 1] != space):
+                    target = True
+                    comp_col = col + 1
+                    print("Below right")
+                    break
+                
+        if target:
+            row = 5
+            while (row >= 0):
+                if board[row][comp_col] == space:
+                    comp_row = row
+                    move = comp_row, comp_col
+                    return move
+                row -= 1
+        print("end of loop?")
+    if not target:
+        print("get random")
+        while True:
+            comp_col = randint(0, 6)
+            row = 5
+            while (row >= 0):
+                if board[row][comp_col] == space:
+                    comp_row = row
+                    move = comp_row, comp_col
+                    return move
+                row -= 1
 
 
 def check_win(board, player):
@@ -80,7 +154,7 @@ def check_win(board, player):
 
 
 def main():
-    BOARD = [[" "] * 7 for x in range(6)]
+    BOARD = [[space] * 7 for x in range(6)]
     print_board(BOARD)
 
     while True:
@@ -102,10 +176,10 @@ def main():
             row, col = get_user_move(BOARD)
             BOARD[row][col] = human
             print_board(BOARD)
-            row, col = get_comp_move(BOARD)
+            row, col = get_comp_move(BOARD, comp)
             BOARD[row][col] = comp
         else:
-            row, col = get_comp_move(BOARD)
+            row, col = get_comp_move(BOARD, comp)
             BOARD[row][col] = comp
             row, col = get_user_move(BOARD)
             BOARD[row][col]
